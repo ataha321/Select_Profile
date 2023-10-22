0073d82221d61487028b5bd8e8be456a016494ff
Ahmed Taha <143041804+ataha321@users.noreply.github.com>
2023-10-22 17:53:02 -0400

    Add files via upload

---
+++ profile.js
@@ -0,0 +1,10 @@
+const setHeading = () =>{
+
+    $("#heading1").text("Ahmed Taha");
+    $("#heading2").text("CIT");
+    $("#heading3").text("Senior");
+
+    $('#heading4').html("<em>Palestine</em>");
+}
+
+$(document).ready(setHeading);
